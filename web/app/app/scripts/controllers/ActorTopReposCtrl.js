b26372d43bce6fa6a0afa3ad6e73331b278f35c7
Chris Dabnor <cdabnor@DabBook-Pro.local>
2016-11-28 22:52:34 +0400

    Implemented actor top repos UI page

--- web/app/app/scripts/controllers/ActorTopReposCtrl.js
+++ web/app/app/scripts/controllers/ActorTopReposCtrl.js
@@ -1,6 +1,33 @@
 'use strict';
 
 angular.module('githubEventsExplorer')
-  .controller('ActorTopReposCtrl', function () {
+  .controller('ActorTopReposCtrl', function ($scope, $http, $mdDialog) {
+    $scope.loading = false;
+    $scope.user = {
+      name: ""
+    };
+    $scope.repos = [];
 
+    $scope.performSearch = function(ev) {
+      $scope.loading = true;
+      $http({
+        method: 'GET',
+        url: '/api/actor/' + $scope.user.name + '/topRepos'
+      }).then(function successCallback(response) {
+        $scope.loading = false;
+        $scope.repos = [response.data];
+      }, function errorCallback(response) {
+        $scope.loading = false;
+        $mdDialog.show(
+          $mdDialog.alert()
+            .parent(angular.element(document.querySelector('.popupContainer')))
+            .clickOutsideToClose(true)
+            .title('Search Error')
+            .textContent('Error performing search: ' + response.data)
+            .ariaLabel('Search Error')
+            .ok('Got it!')
+            .targetEvent(ev)
+        );
+      });
+    };
   });
